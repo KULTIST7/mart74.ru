632b91069c0982493074fcbfca5af65e220688c5
KULTIST7 <kultist77@gmail.com>
2024-04-18 19:50:50 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,57 @@
+$(document).ready(() => {
+    let rellax = new Rellax('.parallax');
+
+    let swiperCare = new Swiper('.swiper-care', {
+        spaceBetween: 20,
+        speed: 750,
+        navigation: {
+            nextEl: '.care__swiper-navigation .swiper-button-next',
+            prevEl: '.care__swiper-navigation .swiper-button-prev'
+        },
+        pagination: {
+            el: '.care__swiper-navigation .swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        },
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true
+        }
+    });
+    
+    Fancybox.bind("[data-fancybox]");
+
+    $('.header__burger-btn').on('click', () => {
+        $('.burger').addClass('burger-open');
+        $('body').addClass('body-noscroll');
+    });
+
+    $('.burger__close-btn').on('click', () => {
+        $('.burger').removeClass('burger-open');
+        $('body').removeClass('body-noscroll');
+    });
+
+    $('.up').on('click', () => {
+        const body = $("html, body");
+        body.animate({scrollTop:0}, 500, 'swing');
+    });
+    
+    $(document).on('scroll', function() {
+        if ($(window).scrollTop() >= 800) {
+            $('.up').removeClass('up-invisible');
+            $('.up').removeClass('up-invisible');
+        } else {
+            $('.up').addClass('up-invisible');
+            $('.up').addClass('up-invisible');
+        }
+    });
+
+    $(document).on('scroll', function() {
+        if ($(window).scrollTop() >= 200) {
+            $('.header').addClass('scrolled');
+        } else {
+            $('.header').removeClass('scrolled');
+        }
+    });
+});
